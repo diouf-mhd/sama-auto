a291503b3c97c8c1fa071b4f7eecbf2d21c22d70
Moussa Diouf <dioufmoussa2.0@outlook.com>
2026-06-28 00:25:09 +0000

    Update ping.ts

--- api/ping.ts
+++ api/ping.ts
@@ -2,8 +2,8 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // On tape directement à la racine de l'API Supabase pour vérifier qu'elle répond
-  const supabaseUrl = "https://oyiiwzbmmsewjwrzjfpd.supabase.co/rest/v1/";
-  const supabaseKey = "sb_publishable_uJnDx-se2jUqGkht6_Rh2A_Vcfddy8D";
+  const supabaseUrl = "https://zzrkitcfzjgvdgvynbrn.supabase.co";
+  const supabaseKey = "sb_publishable_ccQPCKjeZUrgaO91EKXKvQ_uv3BKrvh";
 
   try {
     const response = await fetch(supabaseUrl, {
